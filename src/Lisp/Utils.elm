7a28f993d8473e55794399bb6adffcf180a6a7e5
Rohan Orton <rohan.orton@gmail.com>
2017-04-01 16:00:26 +0100

    fix `both` implementation and add docs

--- src/Lisp/Utils.elm
+++ src/Lisp/Utils.elm
@@ -1,11 +1,32 @@
-module Lisp.Utils exposing ((|||), (&&&))
+module Lisp.Utils exposing (either, (|||), both, (&&&))
+
+{-| Some utility functions.
+-}
 
 
+{-| Returns true if either function evaluates to true.
+-}
+either : (a -> Bool) -> (a -> Bool) -> a -> Bool
+either conditionA conditionB val =
+    conditionA val || conditionB val
+
+
+{-| Infix version of either
+-}
 (|||) : (a -> Bool) -> (a -> Bool) -> a -> Bool
-(|||) conditionA conditionB val =
-    conditionA val || conditionB val
+(|||) =
+    either
 
 
+{-| Returns true if both functions evaluates to true.
+-}
+both : (a -> Bool) -> (a -> Bool) -> a -> Bool
+both conditionA conditionB val =
+    conditionA val && conditionB val
+
+
+{-| Infix version of either
+-}
 (&&&) : (a -> Bool) -> (a -> Bool) -> a -> Bool
-(&&&) conditionA conditionB val =
-    conditionA val || conditionB val
+(&&&) =
+    both
